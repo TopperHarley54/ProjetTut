e0b2bbf02f5043833eb2be565209ec6c66653cc3
TopperHarley54 <fabienbreuil5@gmail.com>
2014-10-19 10:46:17 +0200

    Remise ne place du bootstrap original !
    ON TOUCHE PLUS A BOOTSTRAP, juste style.css

--- Affichage.php
+++ Affichage.php
@@ -240,7 +240,7 @@ function test(){
 				echo ' 								<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
 												</div>
 												<div class="row">
-													<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"></div>
+													<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12"><br><br></div>
 												</div>';								
 			}
 			
