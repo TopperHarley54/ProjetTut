ad8e8832aca318bec9691953d217d4d9755db9fc
TopperHarley54 <fabienbreuil5@gmail.com>
2014-10-19 11:23:58 +0200

    aff
    changement titre signaler promo

--- Affichage.php
+++ Affichage.php
@@ -350,12 +350,14 @@ function test(){
 		}
 		
 		
+    
+    
 		public static function NouvelPromo(){
 			echo '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">';
 			echo '	<form action="NvPromo.php" method="post" enctype="multipart/form-data">
 			  
 						<div class="row">
-							<div class="col-lg-offset-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4 col-lg-2 col-md-2 col-sm-2 col-xs-2"><div id="lastProd">Ajouter un bon plan :</div></div><br><br><br>
+							<div class="col-lg-offset-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4 col-lg-2 col-md-2 col-sm-2 col-xs-2"><div id="lastProd">Ajouter une nouvelle promotion :</div></div><br><br><br>
 								<div class="col-lg-offset-4 col-md-offset-4 col-sm-offset-4 col-xs-offset-4 col-lg-1 col-md-1 col-sm-1 col-xs-1">
 									<label for="cbarre">code barre</label>
 								</div>
