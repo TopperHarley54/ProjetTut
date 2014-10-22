d5ee15ecd8d0ccfcd72c0aeea85691b333d0d752
TopperHarley54 <fabienbreuil5@gmail.com>
2014-10-23 00:57:40 +0200

    af

--- ajout.php
+++ ajout.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once "Base.php";
 $b = Base::getConnection();
     
 
@@ -8,14 +8,16 @@ $prix = isset($_POST['prix']) ? $_POST['prix'] : '';
 $promotion = isset($_POST['promotion']) ? $_POST['promotion'] : '';
 $couleur = isset($_POST['couleur']) ? $_POST['couleur'] : '';
 $taille = isset($_POST['taille']) ? $_POST['taille'] : '';
-$description = isset($_POST['description']) ? $_POST['description'] : ''; 
+$description = isset($_POST['description']) ? $_POST['description'] : '';
+$nomMag = isset($_POST['magasin']) ? $_POST['magasin'] : '';
+$nomVille = isset($_POST['ville']) ? $_POST['ville'] : '';
 
 $flag['code']=0;
 
-$query = $b->prepare("insert into article(nom_article, prix, prix_promo, couleur, taille_dispo, description) values ('$nom' ,'$prix','$promotion','$couleur','$taille','$description') ");
+$query = $b->prepare("insert into article(nom_article, prix, prix_promo, couleur, taille_dispo, description, ville, magasin) values ('$nom' ,'$prix','$promotion','$couleur','$taille','$description','$nomVille','$nomMag') ");
 $dbres = $query->execute();
 print(json_encode($flag));
 
-mysqli_close($connect);
+ //mysql_close();
 
 ?>
